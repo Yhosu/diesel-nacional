d15517ae8a718dff01bcf2604593be445a827bd7
yhosug7 <jgutierrez@solunes.com>
2025-03-04 18:04:45 -0400

    cambios en diesel nacional

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -29,7 +29,8 @@ class Category extends Model {
     }
 
 	public function menus() {
-		return $this->hasMany(Menu::class, 'categoryId', 'id')->where('active', 1);
+		// return $this->hasMany(Menu::class, 'categoryId', 'id')->where('active', 1)->orderBy('order', 'ASC')->limit(5);
+		return $this->hasMany(Menu::class, 'categoryId', 'id')->where('active', 1)->limit(5);
 	}
 
 	public function getLang( $lang ) {

3dc76ec0391b90857e16f1079c48f553506c1380
yhosug7 <jgutierrez@solunes.com>
2025-03-07 20:51:42 -0400

    Cambios en diesel

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -25,7 +25,6 @@ class MainController extends Controller
 		$events			 = \App\Models\Event::where('active', 1)->orderBy('order', 'ASC')->get();
 		$now 			 = date('ymd');
 		$reviews 		 = \Cache::store('database')->remember($now, 86400, function() {
-			\Log::info('aaaaaa');
             $responseSpanish = \Http::get(config('services.tripadvisor.url').'/' . config('services.tripadvisor.merchant_id') . '/reviews?language=es&limit=10&key=' . config('services.tripadvisor.api_key'))->json();
 			$responseEnglish = \Http::get(config('services.tripadvisor.url').'/' . config('services.tripadvisor.merchant_id') . '/reviews?language=en&limit=10&key=' . config('services.tripadvisor.api_key'))->json();
 			$result = array_merge($responseSpanish['data'],$responseEnglish['data']);

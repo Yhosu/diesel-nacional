a68b1ff681a1cf658c2c68f85e733dae58362466
yhosug7 <jgutierrez@solunes.com>
2025-05-03 00:02:29 -0400

    Fix

--- resources/views/includes/menu-items.blade.php
+++ resources/views/includes/menu-items.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
 @foreach ($menuItems as $key => $menuItem)
-<div class="col-md-4" style="margin-right: 5px !important;">
+<div class="col-md-4" style="margin-bottom: 5px !important;">
     <!-- team-item -->
     <div class="team-box">
         <div class="team-photo">

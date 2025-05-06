efd51e4879d4b9f19eaf36c6c464aaea12cebeb0
yhosug7 <jgutierrez@solunes.com>
2025-05-06 01:17:56 -0400

    Cambios en diesel

--- resources/views/includes/menu-items.blade.php
+++ resources/views/includes/menu-items.blade.php
@@ -1,5 +1,3 @@
-
-<div class="gallery-items min-pad  lightgallery three-column fl-wrap" style="margin-bottom:50px;">
 @foreach ($menuItems as $key => $menuItem)
     <div class="gallery-item">
         <div class="grid-item-holder hov_zoom">
@@ -9,7 +7,7 @@
         </div>
     </div>
 @endforeach
-</div>
+
 {{-- <div class="col-md-4" style="margin-bottom: 20px !important;">
     <div class="team-box">
         <div class="team-photo">

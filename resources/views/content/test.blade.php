0b89a963ca62f6d4db2522fedf796a428a3ab920
yhosug7 <jgutierrez@solunes.com>
2025-09-06 13:01:47 -0400

    Fix

--- resources/views/content/test.blade.php
+++ resources/views/content/test.blade.php
@@ -8,8 +8,9 @@
 
     <div>
         <!-- test -->
-<!-- OwnerRez Booking/Inquiry widget for Test Property -->
-<div class="ownerrez-widget" data-propertyId="c831b3307dda416eaf3b5fc8660125fd" data-widget-type="test - Booking/Inquiry" data-widgetId="3dbcbdfc25fa46a79673ed5c44ac2ebd"></div>
+        <!-- test -->
+<!-- OwnerRez Single Month Calendar widget for Test Property -->
+<div class="ownerrez-widget" data-propertyId="c831b3307dda416eaf3b5fc8660125fd" data-widget-type="test - Single Month Calendar" data-widgetId="27ab83edb9a1447287002910520fac4b"></div>
 <script src="https://appstage.ownerrez.com/widget.js"></script>
     </div>
     

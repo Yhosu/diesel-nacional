469e5d88a4e4ecb95a35b8df668bbb53ebb0a483
yhosug7 <jgutierrez@solunes.com>
2025-09-06 13:03:30 -0400

    Fix

--- resources/views/content/test.blade.php
+++ resources/views/content/test.blade.php
@@ -1,17 +1,16 @@
 <x-master-layout>
     <div class="content woocommerce">
-        <div class="ownerrez-widget" data-propertyId="0713b741e1e24a1ca3ddaca3fbdf2906" data-widget-type="Booking/Inquiry" data-widgetId="65ba391d41c44d21914038729acbfa27"></div>
-        <script src="https://app.ownerrez.com/widget.js"></script>
+                <!-- test -->
+        <!-- test -->
+<!-- OwnerRez Single Month Calendar widget for Test Property -->
+<div class="ownerrez-widget" data-propertyId="c831b3307dda416eaf3b5fc8660125fd" data-widget-type="test - Single Month Calendar" data-widgetId="27ab83edb9a1447287002910520fac4b"></div>
+<script src="https://appstage.ownerrez.com/widget.js"></script>
 
         
     </div>
 
     <div>
-        <!-- test -->
-        <!-- test -->
-<!-- OwnerRez Single Month Calendar widget for Test Property -->
-<div class="ownerrez-widget" data-propertyId="c831b3307dda416eaf3b5fc8660125fd" data-widget-type="test - Single Month Calendar" data-widgetId="27ab83edb9a1447287002910520fac4b"></div>
-<script src="https://appstage.ownerrez.com/widget.js"></script>
+
     </div>
     
 </x-master-layout>
